d071db07543b7d61fdcf86dd688f1d6d4b940be1
Natali <ns@numbuster.com>
2024-11-09 13:53:02 +0300

    Initial Commit

---
+++ SnowflakeSUI/SnowflakeSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowflakeSUIApp.swift
+//  SnowflakeSUI
+//
+//  Created by Natalia Sinitsyna on 09.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowflakeSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
